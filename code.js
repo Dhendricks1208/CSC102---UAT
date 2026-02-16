a10c6f46576cfda42605b7121f4e50da4e3828f0
Donneshia <127621004+Dhendricks1208@users.noreply.github.com>
2026-02-16 01:06:06 -0500

    Updated code
    file was in wrong folder not allowing page to be displayed.

--- code.js
+++ code.js
@@ -5,19 +5,15 @@ function updateText(){
 
 // This function helps the game run
 function playGoldFish() {
-
-    // Get random numbers between 1 and 10 for player and computer cards
-    var playerCard = Math.floor(Math.random() * 10) + 1;
-
-    var computerCard = Math.floor(Math.random() * 10) + 1;
-
-    // show cards
-    document.getElementById("gameResult").innerHTML = "You drew a " + playerCard + ". The computer drew a " + computerCard + ".";
-
-    // check if player or computer wins
-    if (playerCard === computerCard) {
-        document.getElementById("gameResult").innerHTML += "<br>You win!";
-    } else {
-        document.getElementById("gameResult").innerHTML += "<br>You lose!";
-    }
+// Get random numbers between 1 and 10 for player and computer cards
+   var playerCard = Math.floor(Math.random() * 5) + 1;
+   var computerCard = Math.floor(Math.random() * 5) + 1;
+    // show cards
+    document.getElementById("gameResult").innerHTML = "You drew a " + playerCard + ". The computer drew a " + computerCard + ".";
+    // check if player or computer wins
+    if (playerCard === computerCard) {
+        document.getElementById("gameResult").innerHTML += "<br>You win!";
+    } else {
+        document.getElementById("gameResult").innerHTML += "<br>You lose!";
+    }
 }
